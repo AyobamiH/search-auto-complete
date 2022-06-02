f1916d8fb43345db858c5215e45b5f6b52324b70
AyobamiH <heryourbarme@live.com>
2022-06-02 21:38:22 +0100

    app.get index search

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const {MongoClient, ObjectId} = require('mongodb')
+const { response } = require('express')
 require('dotenv').config()
 const PORT = process.env.PORT || 3000
 let db,
@@ -17,7 +18,35 @@ MongoClient.connect(dbConnectionStr)
         collection = db.collection('movies')
     })
 
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
 
+app.get('/search', async (req, res) => {
+    try{
+        let result = await  collection.aggregate([
+            {
+                "$Search" : {
+                    "autocomplete":{
+                        "query" : `${request.query.query}`,
+                        "path" : "title",
+                        "fuzzy" : {
+                            "maxEdits": 2,
+                            "prefixLength" : 3
+                        }
+                            
+    
+                    }
+                }
+            }
+        ]).toArray()
+        res.send(result)
+
+    }catch{
+        response.status(500).send({message: error.message})
+
+    }
+})
 
 app.listen(PORT, () =>{
     console.log('SERVER IS RUNNING')
